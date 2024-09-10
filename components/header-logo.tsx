a247cca713d33771742473b6dce5dbde07b44adb
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-09-10 18:10:33 +0530

    header comp

---
+++ components/header-logo.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import Link from "next/link"
+
+
+
+
+export const HeaderLogo = () => {
+    return (
+        <Link href={"/"}>
+            <div className="items-center hidden lg:flex">
+                <Image 
+                    src={"/vittaz-logo-white.svg"}
+                    height={28}
+                    width={28}
+                    alt="Logo"
+                />
+                <p className="font-semibold text-white text-2xl ml-2.5">
+                    Vittaz
+                </p>
+            </div>
+        </Link>
+    )
+}
