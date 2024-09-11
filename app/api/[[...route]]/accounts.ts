354f0dcfd28a05bbf6401e3a1aeb837cf70998b2
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-09-11 19:06:51 +0530

    Accounts query

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,9 +1,19 @@
+import { auth } from "@/auth";
 import { db } from "@/lib/db";
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 
 
 const app = new Hono()
     .get("/" ,async (c) => {
+
+        const session = await auth();
+        if(!session?.user?.id) {
+            throw new HTTPException(401, {
+                res: c.json({error: "Unauthorized"}, 401)
+            })
+        }
+
         const data = await db.accounts.findMany({
             select: {
                 id: true,
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,17 +1,40 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import accounts from "./accounts";
+import { authHandler,initAuthConfig,verifyAuth} from "@hono/auth-js"
 
 import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
+import { HTTPException } from "hono/http-exception";
+import Credentials from "@auth/core/providers/credentials";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
+app.onError((err, c) => {
+    if(err instanceof HTTPException) {
+        return err.getResponse();
+    }
 
+    return c.json({ error: "Internal server error" }, 500)
+});
+
+// Either you can do below or leave as it is - AuthJs middleware will handle it
+app.use("*", initAuthConfig(c=>({
+    secret: c.env.AUTH_SECRET,
+    providers: [
+      Credentials
+    ],
+  })))
+app.use("/api/auth/*", authHandler())
+app.use("/api/*", verifyAuth())
+
+
+// Routing
 const routes = app
     .route("/accounts", accounts);
 
+    
 export const GET = handle(app);
 export const POST = handle(app);
 
