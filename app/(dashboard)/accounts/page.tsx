a98af70efd5e668ab4f66bb8698212096a60e54c
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-09-12 12:53:19 +0530

    confirmation modal

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -13,15 +13,20 @@ import { DataTable } from "@/components/data.table";
 import { columns } from "./columns";
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete";
 
 
 
 
 const AccountsPage = () => {
     const newAccount = useNewAccount();
-    const accountsQuery = useGetAccounts();
 
+    const deleteAccounts = useBulkDeleteAccounts();
+
+    const accountsQuery = useGetAccounts();
     const accounts = accountsQuery.data || [];
+
+    const isDisabled = accountsQuery.isLoading || deleteAccounts.isPending;
 
     if(accountsQuery.isLoading) {
         return (
@@ -57,7 +62,11 @@ const AccountsPage = () => {
                         columns={columns} 
                         data={accounts} 
                         filterKey="email"
-                        onDelete={() => {}}
+                        onDelete={(row) => {
+                            const ids = row.map((r) => r.original.id);
+                            deleteAccounts.mutate({ ids })
+                        }}
+                        disabled={isDisabled}
                     />
                 </CardContent>
             </Card>
--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { AccountsSchema } from "@/prisma/zod";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import * as z from "zod";
 
 
 const app = new Hono()
@@ -46,6 +47,38 @@ const app = new Hono()
             
             return c.json({ data })
     })
+    .post("/bulk-delete",
+        zValidator(
+            "json",
+            z.object({
+                ids: z.array(z.string())
+            })
+        ),
+        async (c) => {
+            const session = await auth();
+            const values = c.req.valid("json")
+
+            if(!session?.user?.id) {
+                return c.json({ error: "Unauthorized" }, 401)
+            }
+
+            const responseData = await db.accounts.deleteMany({
+                where: {
+                    userId: session.user.id,
+                    id: {
+                        in: values.ids
+                    }
+                }
+            });
+
+            const data = values.ids.map((id) => {
+                return {id: id}
+            });
+
+            return c.json({ data: data })
+        
+        }
+    )
 
 
 export default app;
