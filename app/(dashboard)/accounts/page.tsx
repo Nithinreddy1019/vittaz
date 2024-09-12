3a81f03d18a8a66cdfdaafc4a847827ab0d05453
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-09-12 12:12:33 +0530

    Loading state

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -8,29 +8,37 @@ import {
     CardTitle
 } from "@/components/ui/card";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { PlusIcon } from "lucide-react";
+import { LoaderIcon, PlusIcon } from "lucide-react";
 import { DataTable } from "@/components/data.table";
-import { columns, Payment } from "./columns";
+import { columns } from "./columns";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { Skeleton } from "@/components/ui/skeleton";
 
 
-const data: Payment[] = [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    {
-        id: "728ed54f",
-        amount: 500,
-        status: "pending",
-        email: "a@example.com",
-      },
-]
 
 
 const AccountsPage = () => {
     const newAccount = useNewAccount();
+    const accountsQuery = useGetAccounts();
+
+    const accounts = accountsQuery.data || [];
+
+    if(accountsQuery.isLoading) {
+        return (
+            <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+                <Card className="border-none drop-shadow-sm">
+                    <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+                        <Skeleton className="h-8 w-48"/>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="h-[200px] w-full flex items-center justify-center">
+                            <LoaderIcon className="size-4 text-slate-300 animate-spin"/>
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+        )
+    }
 
     return (
         <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -47,7 +55,7 @@ const AccountsPage = () => {
                 <CardContent>
                     <DataTable 
                         columns={columns} 
-                        data={data} 
+                        data={accounts} 
                         filterKey="email"
                         onDelete={() => {}}
                     />
--- components/data.table.tsx
+++ components/data.table.tsx
@@ -137,20 +137,20 @@ export function DataTable<TData, TValue>({
                 {table.getFilteredRowModel().rows.length} row(s) selected.
             </div>
             <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
+                variant="outline"
+                size="sm"
+                onClick={() => table.previousPage()}
+                disabled={!table.getCanPreviousPage()}
             >
-            Previous
+                Previous
             </Button>
             <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
+                variant="outline"
+                size="sm"
+                onClick={() => table.nextPage()}
+                disabled={!table.getCanNextPage()}
             >
-            Next
+                Next
             </Button>
         </div>
     </div>
