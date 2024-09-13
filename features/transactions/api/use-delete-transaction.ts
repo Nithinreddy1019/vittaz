be8f28e089670726c5672077d4e1b7707fce8a96
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-09-13 15:07:59 +0530

    transactions queries

---
+++ features/transactions/api/use-delete-transaction.ts
@@ -0,0 +1,38 @@
+import { InferRequestType } from "hono";
+import { InferResponseType } from "hono";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { client } from "@/lib/hono";
+import { toast } from "sonner";
+
+
+type ResponseType = InferResponseType<typeof client.api.transactions[":id"]["$delete"]>;
+
+
+
+export const useDeleteTransaction = (id?: string) => {
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation<
+        ResponseType,
+        Error
+    >({
+        mutationFn: async () => {
+            const response = await client.api.transactions[":id"]["$delete"]({
+                param: { id }
+            });
+            return await response.json();
+        },
+        onSuccess: () => {
+            toast.success("Account deleted")
+            queryClient.invalidateQueries({ queryKey: ["transaction", { id }] });
+            queryClient.invalidateQueries({ queryKey: ["transactions"] });
+        },
+        onError: () => {
+            toast.error("Failed to delete the transaction")
+        }
+    });
+
+    return mutation;
+}
