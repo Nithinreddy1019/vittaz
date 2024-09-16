22c57f526d3131b9e900a43591449baef5c71cca
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-09-16 19:22:52 +0530

    comps

---
+++ components/amount-input.tsx
@@ -0,0 +1,24 @@
+
+import CurrencyInput from "react-currency-input-field";
+
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger
+} from "@/components/ui/tooltip"
+
+
+
+type Props = {
+    value: string,
+    onchange: (value: string | undefined) => void,
+    placeholder?: string,
+    disabled?: boolean
+};
+
+export const AmoutInput = ({
+    
+}) => {
+
+}
